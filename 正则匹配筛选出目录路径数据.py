08221b5857d1ac6f6d73c32d8e91bc39cb4fbe0d
shantu12 <82096857+shantu123439@users.noreply.github.com>
2023-12-27 15:30:45 +0800

    Add files via upload

---
+++ 正则匹配筛选出目录路径数据.py
@@ -0,0 +1,31 @@
+import os
+import re
+
+# 指定目录和文件名
+directory = r'D:\安服学习笔记\厦门市护网'
+filename = '数据.txt'
+
+# 构建文件路径
+file_path = os.path.join(directory, filename)
+
+# 读取文件内容
+with open(file_path, 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+
+# 提取路径数据
+paths = []
+for line in lines:
+    match = re.search(r"/\s*\w+[\w./-]*", line)  # 使用正则表达式匹配路径类型的数据
+    if match:
+        path = match.group()
+        paths.append(path)
+
+# 导出到txt文件
+output_file = 'result.txt'  # 修改为你想要的输出文件名
+output_path = os.path.join(directory, output_file)
+
+with open(output_path, 'w') as output:
+    for path in paths:
+        output.write(path + '\n')
+
+print('提取完成！')
